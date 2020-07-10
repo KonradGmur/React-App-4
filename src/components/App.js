aede9e24884273fa0c7f938d44ab2f77ab936b05
Konrad Gmur <konrad.gmur@gmail.com>
2020-07-10 18:36:22 +0200

    update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import UsersList from './UsersList'
+import ButtonFetchUsers from './ButtonFetchUsers'
 
+const API = 'https://randomuser.me/api/?results=5';
 class App extends Component {
   state = {
-
+    users: null
   }
+
+  handleDataFetch = () => {
+    // console.log('click');
+    fetch(API)
+      .then(response => {
+        if (response.ok) {
+          return response
+        }
+        throw Error(response.status)
+      })
+      .then()
+      .catch(error => console.log(error + " Ola Boga ale Bug !!!"))
+  }
+
   render() {
-    return ( 
+    const users = this.state.users;
+    return (
       <div>
-        Działa
+        <ButtonFetchUsers click={this.handleDataFetch} />
+        {users ? <UsersList users={users} /> : users}
       </div>
-     );
+    );
   }
 }
 
---
+++ src/components/UsersList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UsersList = (props) => {
+  console.log(props.users);
+
+  return (
+    <ul>
+      <li>Użytkownik 1</li>
+    </ul>
+  );
+}
+
+export default UsersList;
